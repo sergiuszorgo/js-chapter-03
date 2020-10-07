00063c9ea6ea34cf1f2fb235b85466dd28d59441
sergiuszorgo <sergiuszorgo@users.noreply.github.com>
2020-10-07 22:30:52 +0300

    Задача 3-1

---
+++ script.js
@@ -0,0 +1,31 @@
+// Задача 3-1
+let message = "";
+const user = {
+  age: 20,
+  hobby: "html",
+  name: "Mango",
+  premium: true,
+};
+
+//Write code under this line
+user.mood = "happy";
+user["full time"] = true;
+user.hobby = "skydiving";
+user.premium = false;
+
+const keys = Object.keys(user);
+// Write code under this line
+console.log(keys);
+for (const key of keys) {
+  message += `${key} - ${user[key]}\n`;
+}
+
+console.log(message);
+/*
+'age : 20 
+hobby : skydiving 
+name : Mango 
+premium : false 
+mood : happy 
+"full time" : true 
+' */
