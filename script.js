df54b153f042c698f8c73272e8f483e3f9c0539a
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2020-10-07 22:49:25 +0300

    Задача 3-2

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
-// Задача 3-1
-let message = "";
-const user = {
-  age: 20,
-  hobby: "html",
-  name: "Mango",
-  premium: true,
-};
+// // Задача 3-1
+// let message = "";
+// const user = {
+//   age: 20,
+//   hobby: "html",
+//   name: "Mango",
+//   premium: true,
+// };
 
-//Write code under this line
-user.mood = "happy";
-user["full time"] = true;
-user.hobby = "skydiving";
-user.premium = false;
+// //Write code under this line
+// user.mood = "happy";
+// user["full time"] = true;
+// user.hobby = "skydiving";
+// user.premium = false;
 
-const keys = Object.keys(user);
-// Write code under this line
-console.log(keys);
-for (const key of keys) {
-  message += `${key} - ${user[key]}\n`;
-}
+// const keys = Object.keys(user);
+// // Write code under this line
+// console.log(keys);
+// for (const key of keys) {
+//   message += `${key} - ${user[key]}\n`;
+// }
 
-console.log(message);
-/*
-'age : 20 
-hobby : skydiving 
-name : Mango 
-premium : false 
-mood : happy 
-"full time" : true 
-' */
+// console.log(message);
+// /*
+// 'age : 20
+// hobby : skydiving
+// name : Mango
+// premium : false
+// mood : happy
+// "full time" : true
+// ' */
+// // ---
+// Задача 3-2
+// const countProps = function (obj) {
+//   "use strict";
+//   // Write code under this line
+//   const keys = Object.keys(obj);
+//   return keys.length;
+// };
+
+// console.log(countProps({})); // 0
+
+// console.log(countProps({ a: 1, b: 1 })); // 2
+
+// console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 })); // 5
+// ---
