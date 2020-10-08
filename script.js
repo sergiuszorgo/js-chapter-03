19e9d935140d38c16f5e992aa8be06ce5c0c3910
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2020-10-09 00:37:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -113,3 +113,33 @@
 // };
 // console.log(countTotalSalary(supports));
 // // 500
+// Задача 3-5
+function getAllPropValues (array, prop) {
+  'use strict';
+  // Write code under this line 
+    const stack = [];
+    for (const item of array) {
+        stack.push(item.prop)
+    }
+    return stack
+}
+
+// Объекты и ожидаемый результат
+const products = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Радар', price: 1280, quantity: 2 },
+    { name: 'Радар', price: 1320, quantity: 1 },
+    { name: 'Сканер', price: 2700, quantity: 1 },
+    { name: 'Сканер', price: 2500, quantity: 3 },
+    { name: 'Дроид', price: 400, quantity: 7 },
+    { name: 'Захват', price: 1200, quantity: 2 },
+]; 
+
+console.log(getAllPropValues(products, 'name'));
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+
+//console.log(getAllPropValues(products, 'quantity'));
+// [4, 2, 1, 1, 3, 7, 2]
+
+//console.log(getAllPropValues(products, 'category'));
+//  []
