73808cdff049e45bc7d1b1c7828ac5ccd5b1e512
sergiuszorgo <sergiuszorgo@users.noreply.github.com>
2020-10-09 13:40:26 +0300

    Задача 3-5

--- script.js
+++ script.js
@@ -114,33 +114,36 @@
 // console.log(countTotalSalary(supports));
 // // 500
 // Задача 3-5
-function getAllPropValues (array, prop) {
-  'use strict';
-  // Write code under this line 
-    const stack = [];
-    for (const item in array) {
-        // stack.push(item[prop]);
-        console.log(item.prop);
-    }
-    // return stack
-}
+// function getAllPropValues(array, prop) {
+//   "use strict";
+//   // Write code under this line
+//   let stack = [];
+//   for (const item of array) {
+//     for (const key in item) {
+//       if (key === prop) {
+//         stack.push(item[key]);
+//       }
+//     }
+//   }
+//   return stack;
+// }
 
-// Объекты и ожидаемый результат
-const products = [
-    { name: 'Радар', price: 1300, quantity: 4 },
-    { name: 'Радар', price: 1280, quantity: 2 },
-    { name: 'Радар', price: 1320, quantity: 1 },
-    { name: 'Сканер', price: 2700, quantity: 1 },
-    { name: 'Сканер', price: 2500, quantity: 3 },
-    { name: 'Дроид', price: 400, quantity: 7 },
-    { name: 'Захват', price: 1200, quantity: 2 },
-]; 
+// // Объекты и ожидаемый результат
+// const products = [
+//   { name: "Радар", price: 1300, quantity: 4 },
+//   { name: "Радар", price: 1280, quantity: 2 },
+//   { name: "Радар", price: 1320, quantity: 1 },
+//   { name: "Сканер", price: 2700, quantity: 1 },
+//   { name: "Сканер", price: 2500, quantity: 3 },
+//   { name: "Дроид", price: 400, quantity: 7 },
+//   { name: "Захват", price: 1200, quantity: 2 },
+// ];
 
-console.log(getAllPropValues(products, 'name'));
-// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+// console.log(getAllPropValues(products, "name"));
+// // ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
 
-//console.log(getAllPropValues(products, 'quantity'));
-// [4, 2, 1, 1, 3, 7, 2]
+// console.log(getAllPropValues(products, 'quantity'));
+// // [4, 2, 1, 1, 3, 7, 2]
 
-//console.log(getAllPropValues(products, 'category'));
-//  []
+// console.log(getAllPropValues(products, 'category'));
+// //  []
