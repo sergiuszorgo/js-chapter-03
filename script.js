266955c56e8002f0678bd6e633772c3f00f09799
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2020-10-09 23:48:24 +0300

    Пример Репеты - объект и методы работы с ним

--- script.js
+++ script.js
@@ -30,6 +30,7 @@
 // "full time" : true
 // ' */
 // // ---
+
 // Задача 3-2
 // const countProps = function (obj) {
 //   "use strict";
@@ -44,6 +45,7 @@
 
 // console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 })); // 5
 // ---
+
 // Задача 3-3
 // const findBestEmployee = function (employees) {
 //   "use strict";
@@ -86,6 +88,7 @@
 // console.log(findBestEmployee(sellers));
 // // 'lux'
 // ---
+
 // Задача 3-4
 // const countTotalSalary = function (employees) {
 //   "use strict";
@@ -113,37 +116,123 @@
 // };
 // console.log(countTotalSalary(supports));
 // // 500
+
 // Задача 3-5
-function getAllPropValues(array, prop) {
-  "use strict";
-  // Write code under this line
-  let stack = [];
-    for (const item of array) {
-    for (const key in item) {
-      if (key === prop) {
-        stack.push(item[key]);
-      }
-    }
-  }
-  return stack;
-}
+// function getAllPropValues(array, prop) {
+//   "use strict";
+//   // Write code under this line
+//   let stack = [];
+//     for (const item of array) {
+//     for (const key in item) {
+//       if (key === prop) {
+//         stack.push(item[key]);
+//       }
+//     }
+//   }
+//   return stack;
+// }
 
-// Объекты и ожидаемый результат
-const products = [
-  { name: "Радар", price: 1300, quantity: 4 },
-  { name: "Радар", price: 1280, quantity: 2 },
-  { name: "Радар", price: 1320, quantity: 1 },
-  { name: "Сканер", price: 2700, quantity: 1 },
-  { name: "Сканер", price: 2500, quantity: 3 },
-  { name: "Дроид", price: 400, quantity: 7 },
-  { name: "Захват", price: 1200, quantity: 2 },
-];
+// // Объекты и ожидаемый результат
+// const products = [
+//   { name: "Радар", price: 1300, quantity: 4 },
+//   { name: "Радар", price: 1280, quantity: 2 },
+//   { name: "Радар", price: 1320, quantity: 1 },
+//   { name: "Сканер", price: 2700, quantity: 1 },
+//   { name: "Сканер", price: 2500, quantity: 3 },
+//   { name: "Дроид", price: 400, quantity: 7 },
+//   { name: "Захват", price: 1200, quantity: 2 },
+// ];
 
-console.log(getAllPropValues(products, "name"));
-// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+// console.log(getAllPropValues(products, "name"));
+// // ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
 
-console.log(getAllPropValues(products, 'quantity'));
-// [4, 2, 1, 1, 3, 7, 2]
+// console.log(getAllPropValues(products, 'quantity'));
+// // [4, 2, 1, 1, 3, 7, 2]
 
-console.log(getAllPropValues(products, 'category'));
+// console.log(getAllPropValues(products, 'category'));
 //  []
+
+// - - -
+
+// Пример Репеты - объект и методы работы с ним
+
+// const cart = {
+//   items: [{name: 'apple', price: 50, quantity: 1}],
+//   getItems() {
+//     return this.items;
+//   },
+//   add(product) {
+//     for (const item of this.items) {
+//       if (item.name === product.name) {
+//         console.log('такой продукт уже есть, увеличиваем кол-во');
+//         item.quantity += 1;
+//         return;
+//       }
+//     }
+//     console.log('Новый продукт, добавляем в корзину');
+//     product.quantity = 1;
+//     this.items.push(product);
+//   },
+//   remove(productName) {
+//     for (let i = 0; i < this.items.length; i += 1) {
+//       const item = this.items[i];
+
+//       if (productName === item.name) {
+//         console.log('Нашли, удаляем! Индех:', i);
+//         this.items.splice(i, 1);
+//       }
+//     }
+//   },
+//   clear() {
+//     this.items = [];
+//   },
+//   countTotalPrice() {
+//     let total = 0;
+//     for (const item of this.items) {
+//       total += item.price * item.quantity;
+//     }
+
+//     return total;
+//   },
+//   increaseQuantity(productName) {
+//     for (const item of this.items) {
+//       if (item.name === productName) {
+//         item.quantity += 1;
+//       }
+//     }
+//   },
+//   decreaseQuantity(productName) {
+//     for (const item of this.items) {
+//       if (item.name === productName) {
+//         if (item.quantity - 1 === 0) {
+//           this.remove(productName);
+//           return;
+//         }
+
+//         item.quantity -= 1;
+//       }
+//     }
+//   },
+// };
+
+// console.table(cart.getItems());
+
+// cart.add({ name: 'apple', price: 50 });
+// cart.add({ name: 'lemon', price: 60 });
+// cart.add({ name: 'lemon', price: 60 });
+// console.table(cart.getItems());
+
+// cart.remove('apple');
+// console.table(cart.getItems());
+
+// cart.clear();
+// console.table(cart.getItems());
+
+// cart.increaseQuantity('apple');
+// console.table(cart.getItems());
+
+// cart.decreaseQuantity('lemon');
+// cart.decreaseQuantity('lemon');
+// console.table(cart.getItems());
+
+// console.log('Total: ', cart.countTotalPrice());
